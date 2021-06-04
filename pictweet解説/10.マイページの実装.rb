1f9c1aa903e55929c61ce28ee98732084ff68888
sojiro1208 <80874997+sojiro1208@users.noreply.github.com>
2021-06-04 09:13:20 +0900

    mergeメソッド

---
+++ pictweet解説/10.マイページの実装.rb
@@ -0,0 +1,10 @@
+current_userメソッド：現在ログインしているユーザー情報を取得できる。
+
+mergeメソッド：ハッシュを統合させるときに使用するメソッド
+【例】2つのハッシュを1つにまとめることができるのだ！
+  tweet = { name: "たなか", text: "test", image: "test.jpeg" }
+  uid = { user_id: "1" }
+  tweet.merge(uid)
+  => {:name=>"たなか", :text=>"test", :image=>"test.jpeg", :user_id=>"1"}
+
+
