d11b608c77b82289d1f5613184da212373db8af4
sojiro1208 <80874997+sojiro1208@users.noreply.github.com>
2021-06-05 13:36:34 +0900

    検索機能です

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,4 +2,12 @@ class Tweet < ApplicationRecord
   validates :text, presence: true
   belongs_to :user
   has_many :comments
+
+  def self.search(search)
+    if seach != ""
+      Tweet.where('text LIKE(?)', "%#{search}%")
+    else
+      TWeet.all
+    end
+  end
 end
--- pictweet解説/14.投稿検索機能.rb
+++ pictweet解説/14.投稿検索機能.rb
@@ -24,4 +24,21 @@ searchアクションのルーティング設定
     【例】memberのルーティング
       Prefix           Verb    URI                                 Pattern
       search_tweet      GET    /tweets/:id/search(.:format)       tweets#search
-      
+whereメソッド：モデルが使用できるActiveRecordメソッドの１つ
+  【例】whereメソッド：テーブルないの条件に一致したレコードのインスタンスを配列の形で取得できる
+    モデル.where('検索対象となるカラムを含む条件式')
+LIKE句：曖昧な文字列を検索するときに使用。whereメソッドと一緒に使う
+  [例]「1文字目に'a'という文字列が入ったデータ」「最後の文字に'b'が入っているデータ」「文字列の途中に'c'が入ったデータ」など
+  
+  文字列：意味
+  %    ：任意の文字列（空白文字列含む）
+  _    ：任意の1文字
+
+  実行サンプル
+  実行例                       ：詳細
+  where('title LIKE(?)', "a%")：aから始まるタイトル
+  where('title LIKE(?)', "%b")：bで終わるタイトル
+  where('title LIKE(?)', "%c%")：cが含まれるタイトル
+  where('title LIKE(?)', "d_")：dで始まる2文字のタイトル
+  where('title LIKE(?)', "_e")：eで終わる2文字のタイトル
+  
